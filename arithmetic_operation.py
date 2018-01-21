59467136427127894c050ed33668936090465d17
Akash <akashdhamanekar07@gmail.com>
2018-01-21 16:07:43 +0530

    Arithmetic operations

---
+++ arithmetic_operation.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    print("Enter Number: ")
+    a = int(raw_input())
+    print("Enter Number: ")
+    b = int(raw_input())
+    print("addition of %s, %s numbers = %s"%(a, b, a + b))
+    print(a-b)
+    print(a*b)
